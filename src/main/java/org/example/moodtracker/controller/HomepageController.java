b5d70d3bbee5f8edbfe0338a8b87a8557e20b0c6
ltgford <n11229110@qut.edu.au>
2024-04-30 09:28:20 +1000

    updated homepage controller & fxml with profile page buttons

--- src/main/java/org/example/moodtracker/controller/HomepageController.java
+++ src/main/java/org/example/moodtracker/controller/HomepageController.java
@@ -33,6 +33,8 @@ public class HomepageController implements Initializable {
     @FXML
     private Button button_table;
     @FXML
+    private Button button_profile;
+    @FXML
     private Label label_welcome;
     @FXML
     private Label current_date;
@@ -60,6 +62,7 @@ public class HomepageController implements Initializable {
         button_logout.setOnAction(event -> DBUtils.changeScene(event, "login.fxml", "Log In", null));
         button_close.setOnAction(actionEvent -> UIUtils.closeApp((Stage) button_close.getScene().getWindow()));
         button_table.setOnAction(event -> DBUtils.changeScene(event, "tableView.fxml", "Table View", null));
+        button_profile.setOnAction(event -> DBUtils.changeScene(event,"profile.fxml","Profile",null));
 
         // Set user information and current date
         UIUtils.setUserInformation(label_welcome, "Username Goes Here");
--- src/main/java/org/example/moodtracker/controller/ProfileSettingsController.java
+++ src/main/java/org/example/moodtracker/controller/ProfileSettingsController.java
@@ -3,21 +3,28 @@ package org.example.moodtracker.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import org.example.moodtracker.model.DBUtils;
+import org.example.moodtracker.model.UIUtils;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ProfileSettingsController implements Initializable {
+
     @FXML
     private Button button_logout;
-    @FXML
-    private Button button_close;
     @FXML
     private Button button_table;
     @FXML
     private Button button_homepage;
+    @FXML
+    private Button button_close;
+    @FXML
+    private Label label_welcome;
+    @FXML
+    private Label current_date;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -25,14 +32,10 @@ public class ProfileSettingsController implements Initializable {
         button_homepage.setOnAction(event -> DBUtils.changeScene(event,"homepage.fxml", "Home",null));
         button_logout.setOnAction(event -> DBUtils.changeScene(event, "login.fxml", "Log In", null));
         button_table.setOnAction(event -> DBUtils.changeScene(event, "tableView.fxml", "Table View", null));
+        button_close.setOnAction(actionEvent -> UIUtils.closeApp((Stage) button_close.getScene().getWindow()));
+
+        // Set user information and current date
+        UIUtils.setUserInformation(label_welcome, "Username Goes Here");
+        UIUtils.setCurrentDate(current_date);
     }
-
-    // Method to close the entire program
-    private void closeApp() {
-        Stage stage = (Stage) button_close.getScene().getWindow();
-        stage.close();
-    }
-
-
-
 }
