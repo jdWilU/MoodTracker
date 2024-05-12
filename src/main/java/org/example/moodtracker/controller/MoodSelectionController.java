838b6bb55b7b8f69fbf15589fd93cecc81cf35d2
SagarSuresh002 <sagar.suresh002@gmail.com>
2024-05-13 00:28:43 +1000

    Reverted to when the buttons working

--- src/main/java/org/example/moodtracker/controller/MoodSelectionController.java
+++ src/main/java/org/example/moodtracker/controller/MoodSelectionController.java
@@ -2,8 +2,7 @@ package org.example.moodtracker.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+
 import org.example.moodtracker.model.DBUtils;
 
 public class MoodSelectionController {
@@ -12,21 +11,8 @@ public class MoodSelectionController {
     private Button button_submit;
 
     @FXML
-    private RadioButton rb1, rb2, rb3, rb4, rb5;
-
-    private ToggleGroup group;
-
-    @FXML
     public void initialize() {
-        // Initialize the ToggleGroup
-        group = new ToggleGroup();
-        rb1.setToggleGroup(group);
-        rb2.setToggleGroup(group);
-        rb3.setToggleGroup(group);
-        rb4.setToggleGroup(group);
-        rb5.setToggleGroup(group);
-
-        // Configure the submit button action
+        // Configure the submit button action to change scenes
         button_submit.setOnAction(event -> DBUtils.changeScene(event, "activity.fxml", "Activity Selection", null));
     }
 }
