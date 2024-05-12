f21f9baa0a06bd81162b75cde8c329258c188ee1
SagarSuresh002 <sagar.suresh002@gmail.com>
2024-05-12 16:18:06 +1000

    Connected the submit button in the mood selection page to the activity selection page

--- src/main/java/org/example/moodtracker/controller/MoodSelectionController.java
+++ src/main/java/org/example/moodtracker/controller/MoodSelectionController.java
@@ -1,4 +1,18 @@
 package org.example.moodtracker.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import org.example.moodtracker.model.DBUtils;
+
 public class MoodSelectionController {
+
+    @FXML
+    private Button button_submit;
+
+    @FXML
+    public void initialize() {
+        // Configure the submit button action to change scenes
+        button_submit.setOnAction(event -> DBUtils.changeScene(event, "activity.fxml", "Activity Selection", null));
+    }
 }
