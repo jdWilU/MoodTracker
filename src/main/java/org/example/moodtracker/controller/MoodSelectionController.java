5affd55055300780f8de27b43de512513b7fef54
SagarSuresh002 <sagar.suresh002@gmail.com>
2024-05-13 00:07:50 +1000

    changed to radio button for mood selection page so user can only select one mood

--- src/main/java/org/example/moodtracker/controller/MoodSelectionController.java
+++ src/main/java/org/example/moodtracker/controller/MoodSelectionController.java
@@ -2,7 +2,8 @@ package org.example.moodtracker.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import org.example.moodtracker.model.DBUtils;
 
 public class MoodSelectionController {
@@ -11,8 +12,21 @@ public class MoodSelectionController {
     private Button button_submit;
 
     @FXML
+    private RadioButton rb1, rb2, rb3, rb4, rb5;
+
+    private ToggleGroup group;
+
+    @FXML
     public void initialize() {
-        // Configure the submit button action to change scenes
+        // Initialize the ToggleGroup
+        group = new ToggleGroup();
+        rb1.setToggleGroup(group);
+        rb2.setToggleGroup(group);
+        rb3.setToggleGroup(group);
+        rb4.setToggleGroup(group);
+        rb5.setToggleGroup(group);
+
+        // Configure the submit button action
         button_submit.setOnAction(event -> DBUtils.changeScene(event, "activity.fxml", "Activity Selection", null));
     }
 }
