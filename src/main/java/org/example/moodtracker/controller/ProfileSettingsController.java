11182b16b2a207a04db9b0b1a6774300749b69c1
ltgford <n11229110@qut.edu.au>
2024-05-31 20:04:07 +1000

    profilesettings controller code cleanup

--- src/main/java/org/example/moodtracker/controller/ProfileSettingsController.java
+++ src/main/java/org/example/moodtracker/controller/ProfileSettingsController.java
@@ -5,18 +5,13 @@ import io.github.palexdev.materialfx.controls.MFXProgressBar;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.example.moodtracker.model.DBUtils;
 import org.example.moodtracker.model.UIUtils;
 import org.example.moodtracker.model.UserInfo;
 
-import javax.swing.text.html.ImageView;
-import java.awt.*;
 import java.net.URL;
-import java.sql.*;
+import java.sql.SQLException;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -77,59 +72,48 @@ public class ProfileSettingsController implements Initializable {
     @FXML
     private MFXProgressBar resources_visited;
 
-
-    private static final String[] COLORS = {
-            "#fe6969", // Red
-            "#a364f8", // Purple
-            "#838383", // Gray
-            "#2cb2ff", // Blue
-            "#20e49f"  // Green
-    };
+    private static final Logger LOGGER = Logger.getLogger(ProfileSettingsController.class.getName());
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Button functionality
-        button_homepage.setOnAction(event -> DBUtils.changeScene(event,"homepage.fxml", "Home",null));
-        button_logout.setOnAction(event -> DBUtils.changeScene(event, "login.fxml", "Log In", null));
-        button_table.setOnAction(event -> DBUtils.changeScene(event, "tableView.fxml", "Table View", null));
-        button_close.setOnAction(actionEvent -> UIUtils.closeApp((Stage) button_close.getScene().getWindow()));
-        button_daily_entry.setOnAction(event -> DBUtils.changeScene(event, "mood-tracking-page.fxml", "Mood Tracking", null));
-        button_resources.setOnAction(event -> DBUtils.changeScene(event, "resources-page.fxml", "Educational Resources", null));
-
-        button_save.setOnAction(event -> updateUser());
-        button_delete.setOnAction(event -> handleDeleteAccount());
-
-        // Retrieve current user information
+        setupButtonActions();
         String currentUser = DBUtils.getCurrentUsername();
+
         if (currentUser != null) {
             UIUtils.setUserInformation(label_welcome, currentUser);
             try {
                 int userId = DBUtils.getUserId(currentUser);
                 boolean entryExists = DBUtils.entryExistsForToday(userId);
-                updateDailyEntryProgress(entryExists); // Update daily entry progress based on whether the entry exists for today
+                updateDailyEntryProgress(entryExists);
             } catch (SQLException e) {
-                Logger.getLogger(ProfileSettingsController.class.getName()).log(Level.SEVERE, "Error checking today's entry", e);
+                LOGGER.log(Level.SEVERE, "Error checking today's entry", e);
             }
         }
 
-        // Set current date
         UIUtils.setCurrentDate(current_date);
-
-        // Populate user information
-        UserInfo user = DBUtils.getUserInfo(DBUtils.getCurrentUsername());
-        if (user != null) {
-            tf_username.setText(user.getUsername());
-            tf_email.setText(user.getEmail());
-            pw_password.setText(user.getPassword());
-            tf_phone.setText(user.getPhoneNumber());
-            tf_displayname.setText(user.getDisplayName());
-        }
-
-        //Initialise the user level and achievements
-        // Initialize level and XP information
+        populateUserInfo();
         initializeLevelAndXP();
         initializeStreakCount();
         initialiseResourcesAchievement();
+    }
+
+    private void setupButtonActions() {
+        button_homepage.setOnAction(event -> DBUtils.changeScene(event, "homepage.fxml", "Home", null));
+        button_logout.setOnAction(event -> DBUtils.changeScene(event, "login.fxml", "Log In", null));
+        button_table.setOnAction(event -> DBUtils.changeScene(event, "tableView.fxml", "Table View", null));
+        button_close.setOnAction(actionEvent -> UIUtils.closeApp((Stage) button_close.getScene().getWindow()));
+        button_daily_entry.setOnAction(event -> DBUtils.changeScene(event, "mood-tracking-page.fxml", "Mood Tracking", null));
+        button_resources.setOnAction(event -> DBUtils.changeScene(event, "resources-page.fxml", "Educational Resources", null));
+        button_save.setOnAction(event -> updateUser());
+        button_delete.setOnAction(event -> handleDeleteAccount());
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String content) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 
     private void updateUser() {
@@ -144,25 +128,15 @@ public class ProfileSettingsController implements Initializable {
             UserInfo userInfo = DBUtils.getUserInfo(currentUser);
             if (userInfo != null) {
                 DBUtils.updateUserInfo(currentUser, newUsername, newEmail, newPassword, newDisplayName, newPhoneNumber);
-
                 if (!currentUser.equals(newUsername)) {
                     DBUtils.setCurrentUsername(newUsername);
                 }
-
                 showAlert(Alert.AlertType.INFORMATION, "Update Successful", "Your information has been updated successfully.");
             }
         } catch (SQLException e) {
-            Logger.getLogger(ProfileSettingsController.class.getName()).log(Level.SEVERE, "Error updating user information", e);
+            LOGGER.log(Level.SEVERE, "Error updating user information", e);
             showAlert(Alert.AlertType.ERROR, "Error", "An error occurred while updating your information. Please try again later.");
         }
-    }
-
-    private void showAlert(Alert.AlertType alertType, String title, String content) {
-        Alert alert = new Alert(alertType);
-        alert.setTitle(title);
-        alert.setHeaderText(null);
-        alert.setContentText(content);
-        alert.showAndWait();
     }
 
     private void handleDeleteAccount() {
@@ -171,16 +145,15 @@ public class ProfileSettingsController implements Initializable {
         alert.setHeaderText("Delete Account");
         alert.setContentText("Are you sure you want to delete your account? This action cannot be undone.");
         Optional<ButtonType> result = alert.showAndWait();
+
         if (result.isPresent() && result.get() == ButtonType.OK) {
             String username = DBUtils.getCurrentUsername();
             try {
                 DBUtils.deleteUser(username);
                 UIUtils.logout();
-
             } catch (SQLException e) {
-                Logger.getLogger(ProfileSettingsController.class.getName()).log(Level.SEVERE, "Error deleting user account", e);
-
-                showAlert(Alert.AlertType.ERROR,"Error","An error occurred while deleting your account. Please try again later.");
+                LOGGER.log(Level.SEVERE, "Error deleting user account", e);
+                showAlert(Alert.AlertType.ERROR, "Error", "An error occurred while deleting your account. Please try again later.");
             }
         }
     }
@@ -190,7 +163,6 @@ public class ProfileSettingsController implements Initializable {
         if (currentUser != null) {
             try {
                 int userId = DBUtils.getUserId(currentUser);
-
                 int xp = DBUtils.getXpForUser(userId);
                 int level = DBUtils.getUserLevel(userId);
                 int xpToNextLevel = ((level + 1) * 100) - xp;
@@ -207,64 +179,45 @@ public class ProfileSettingsController implements Initializable {
                 xpLevel.setProgress(progress);
                 xpLevelTopBar.setProgress(progress);
             } catch (SQLException e) {
-                Logger.getLogger(ProfileSettingsController.class.getName()).log(Level.SEVERE, "Error fetching user's level and XP", e);
+                LOGGER.log(Level.SEVERE, "Error fetching user's level and XP", e);
                 showAlert(Alert.AlertType.ERROR, "Error", "An error occurred while fetching user's level and XP. Please try again later.");
             }
         }
     }
 
     private void updateDailyEntryProgress(boolean entryExists) {
-        if (entryExists) {
-            barDailyEntry.setProgress(1.0);
-        } else {
-            barDailyEntry.setProgress(0.0);
-        }
+        barDailyEntry.setProgress(entryExists ? 1.0 : 0.0);
     }
+
     private void initializeStreakCount() {
         String currentUser = DBUtils.getCurrentUsername();
         if (currentUser != null) {
             try {
-                // Get user ID
                 int userId = DBUtils.getUserId(currentUser);
-
-                // Fetch user's longest streak from the database
                 int longestStreak = DBUtils.getUserEntryHistory(userId);
                 int totalEntries = DBUtils.getTotalEntries(userId);
 
-                // Set the progress for the 3-day streak progress bar
-                if (totalEntries >= 3) {
-                    if (longestStreak >= 3) {
-                        three_day_streak.setProgress(1.0); // 100%
-                        int xpToAdd = 15;
-                        DBUtils.updateXpInDatabase(userId, xpToAdd);
-                        DBUtils.updateLevelInDatabase(userId);
-                    } else {
-                        three_day_streak.setProgress((double) totalEntries / 3);
-                    }
-                } else {
-                    three_day_streak.setProgress((double) totalEntries / 3);
-                }
-
-                // Set the progress for the 5-day streak progress bar
-                if (totalEntries >= 5) {
-                    if (longestStreak >= 5) {
-                        five_day_streak.setProgress(1.0); // 100%
-                        int xpToAdd = 50;
-                        DBUtils.updateXpInDatabase(userId, xpToAdd);
-                        DBUtils.updateLevelInDatabase(userId);
-                    } else {
-                        five_day_streak.setProgress((double) totalEntries / 5);
-                    }
-                } else {
-                    five_day_streak.setProgress((double) totalEntries / 5);
-                }
+                updateStreakProgress(three_day_streak, totalEntries, longestStreak, 3, userId, 15);
+                updateStreakProgress(five_day_streak, totalEntries, longestStreak, 5, userId, 50);
 
             } catch (SQLException e) {
-                // Handle SQLException
-                Logger.getLogger(ProfileSettingsController.class.getName()).log(Level.SEVERE, "Error fetching user's streak count", e);
-                // Show error message to the user
+                LOGGER.log(Level.SEVERE, "Error fetching user's streak count", e);
                 showAlert(Alert.AlertType.ERROR, "Error", "An error occurred while fetching user's streak count. Please try again later.");
             }
+        }
+    }
+
+    private void updateStreakProgress(MFXProgressBar streakBar, int totalEntries, int longestStreak, int streakGoal, int userId, int xpToAdd) throws SQLException {
+        if (totalEntries >= streakGoal) {
+            if (longestStreak >= streakGoal) {
+                streakBar.setProgress(1.0);
+                DBUtils.updateXpInDatabase(userId, xpToAdd);
+                DBUtils.updateLevelInDatabase(userId);
+            } else {
+                streakBar.setProgress((double) totalEntries / streakGoal);
+            }
+        } else {
+            streakBar.setProgress((double) totalEntries / streakGoal);
         }
     }
 
@@ -272,24 +225,25 @@ public class ProfileSettingsController implements Initializable {
         String currentUser = DBUtils.getCurrentUsername();
         if (currentUser != null) {
             try {
-                // Get user ID
                 int userId = DBUtils.getUserId(currentUser);
-
-                // Check if user has visited the education page
                 boolean visitedEducation = DBUtils.hasVisitedEducation(userId);
-
-                // Set progress for the resource_visited progress bar
-                if (visitedEducation) {
-                    resources_visited.setProgress(1.0); // 100%
-                } else {
-                    resources_visited.setProgress(0.0); // 0%
-                }
-
+                resources_visited.setProgress(visitedEducation ? 1.0 : 0.0);
             } catch (SQLException e) {
-                // Handle SQLException
-                System.err.println("Error checking visited_education column: " + e.getMessage());
+                LOGGER.log(Level.SEVERE, "Error checking visited_education column", e);
                 showAlert(Alert.AlertType.ERROR, "Error", "An error occurred while checking your resource visit status. Please try again later.");
             }
+        }
+    }
+
+    private void populateUserInfo() {
+        String currentUser = DBUtils.getCurrentUsername();
+        UserInfo user = DBUtils.getUserInfo(currentUser);
+        if (user != null) {
+            tf_username.setText(user.getUsername());
+            tf_email.setText(user.getEmail());
+            pw_password.setText(user.getPassword());
+            tf_phone.setText(user.getPhoneNumber());
+            tf_displayname.setText(user.getDisplayName());
         }
     }
 }
